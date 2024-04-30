d3434aac194711f941929bf3be1fc68cd6e65af5
park ki chan <13478qw@naver.com>
2024-04-30 17:11:14 +0900

    9주차 제출

--- src/com/section04/Ex1012.java
+++ src/com/section04/Ex1012.java
@@ -1,9 +1,15 @@
 package com.section04;
 
+import java.util.StringTokenizer;
+
 public class Ex1012 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		StringTokenizer st = new StringTokenizer("Java", "C", "Python", "JSP", "PHP");
+				while(st.hasMoreTokens()) {
+					System.out.println(st.nextToken());
+				}
+		
 
 	}
 
